61b7d23443eb3ec32766acf1d6ef316d3c49b5bf
Jhonaiker Blanco <blancojhonaiker@gmail.com>
2023-03-23 20:19:26 -0400

    A punto de hacer cambios esotericos

--- main.c
+++ main.c
@@ -14,105 +14,146 @@
  * -> Idea General:
  *
  *
-*/
+ */
 
 #include "standard_lib.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 	// Verificacion de argumentos de entrada.
-	if(argc > 1 && argc < 5){ 
-		open_files(argc, argv);    // Apertura de archivos.
-		initial_structs();         // Inicializacion de estructuras.
-		
-		ReadCacCharge();           // Lectura del archivo de carga.
-		ReadCacService();          // Lectura del archivo de servicios.
-	
-		// Arreglo de PID de cada proceso creado.
-		int child_pids[num_of_process+2];
+	if (argc > 1 && argc < 5)
+	{
+		open_files(argc, argv); // Apertura de archivos.
+		initial_structs();		// Inicializacion de estructuras.
 
-		// Inicializar array que controla cantidad de buses usados, actualmente usado y que ya cumplieron su ruta
-		FOR(n,1,num_of_process + 2){
-			amountOfBusesUsedByRoute[n] = 0;
-			amountOfBusesFinishedByRoute[n] = 0;
-			amountOfBusesCurrentlyUsedByRoute[n] = 0;
-		}
-		
-		/*FOR(n,1, num_of_process + 2){
-			FOR(k,1,num_of_process + 2){
-				total_ser[n_routes][max_bus].code == total_cha[].code
-			}
-		}*/
+		ReadCacCharge();  // Lectura del archivo de carga.
+		ReadCacService(); // Lectura del archivo de servicios.
+
+		// Arreglo de PID de cada proceso creado.
+		int child_pids[num_of_process + 2];
 
 		// Arreglo de files descriptos o bien sea PIPES.
-		int files_desc[num_of_process+2][2];
-		
+		int files_desc[num_of_process + 2][2];
+
 		// Apertura de todos los pipes a usar.
-		FOR(n, 1, num_of_process+2) pipe(files_desc[n]);
-	
+		FOR(n, 1, num_of_process + 2)
+		pipe(files_desc[n]);
+
 		// Creacion de procesos hijos.
-		FOR(n, 1, num_of_process){
+		FOR(n, 1, num_of_process)
+		{
 			PROCESS_ID++;
 			child_pids[PROCESS_ID] = fork();
-			if(!child_pids[PROCESS_ID]) break;
+			if (!child_pids[PROCESS_ID])
+				break;
 		}
 
-		FOR(n, 1, num_of_process){
+		// Inicializar array que controla cantidad de buses usados, actualmente usado y que ya cumplieron su ruta
+		FOR(n, 1, num_of_process + 1)
+		{
 			amountOfBusesUsedByRoute[n] = 0;
+			amountOfBusesFinishedByRoute[n] = 0;
+			amountOfBusesGoingToBusStation[n] = 0;
+			amountOfBusesGoingToUniversity[n] = 0;
+			amountOfBusesWaitingForPeople[n] = 0;
 		}
-		
-		if(!child_pids[PROCESS_ID]){ // PROCESOS HIJOS.
-			child_funtion(PROCESS_ID, files_desc);
 
-		} else { // PROCESO PADRE.
-			close(files_desc[1][0]); 	// Cerramos primer pipe de lectura.
-			close(files_desc[PROCESS_ID+1][1]); // Cerramos el ultimo pipe escritura.
+		FOR(i, 1, num_of_process + 2)
+		{
+			FOR(j, 1, num_of_process + 2)
+			{
+				if (strcmp(total_ser[i][1].code, total_cha[j].code) == 0)
+				{
+
+					FOR(k, 1, max_bus)
+					{
+						total_ser[i][k].travel_time = total_cha[j].min_travel;
+						total_ser[i][k].charge_id = j;
+					}
+
+					servicePositionInMatrixByRoute[j] = i;
+					routePositionInMatrixByService[i] = j;
+					break;
+				}
+			}
+		}
+
+		FOR(i, 1, num_of_process + 2)
+		{
+			FOR(j, 1, max_bus)
+			{
+				total_ser[i][j].numberOfBusInRoute = j;
+				total_ser[i][j].progressPercentage = 0;
+				total_ser[i][j].isWaitingForPeople = 0;
+				total_ser[i][j].isReturningToUniversity = 0;
+			}
+		}
+
+		if (!child_pids[PROCESS_ID])
+		{ // PROCESOS HIJOS.
+			child_funtion(PROCESS_ID, files_desc);
+		}
+		else
+		{										  // PROCESO PADRE.
+			close(files_desc[1][0]);			  // Cerramos primer pipe de lectura.
+			close(files_desc[PROCESS_ID + 1][1]); // Cerramos el ultimo pipe escritura.
 			char buf[10], buf1[10];
 			int minutes = 0;
 			struct timeval tm_begin, tm_end;
-			long t_ms = 1e6*Min_Simul;
+			long t_ms = 1e6 * Min_Simul;
 			long curr_tms = 0;
 			long diff = 0;
 			long scs = 0;
-			
+
 			int cnt = 0;
-			
-			while(TRUE){
+
+			while (TRUE)
+			{
 				tracker_hour(Hour_Simul);
 				sprintf(buf, "%d", Hour_Simul);
-				if(Hour_Simul == Hour_Final) {
+				if (Hour_Simul == Hour_Final)
+				{
 					write(files_desc[1][1], "-1", 3);
-				} else {
+				}
+				else
+				{
 					gettimeofday(&tm_begin, NULL);
 					write(files_desc[1][1], buf, 10);
 				}
-				read(files_desc[PROCESS_ID+1][0], buf1, 10);
+				read(files_desc[PROCESS_ID + 1][0], buf1, 10);
 				gettimeofday(&tm_end, NULL);
 				sscanf(buf1, "%d", &minutes);
-				if(minutes == -1) break;
+				if (minutes == -1)
+					break;
 				scs = (tm_end.tv_sec - tm_begin.tv_sec);
-				curr_tms = (scs*1e6)+tm_end.tv_usec-tm_begin.tv_usec;
-				
-				if(curr_tms < t_ms){
+				curr_tms = (scs * 1e6) + tm_end.tv_usec - tm_begin.tv_usec;
+
+				if (curr_tms < t_ms)
+				{
 					t_ms -= curr_tms;
 					gettimeofday(&tm_begin, NULL);
 					usleep(t_ms);
 					Hour_Simul++;
 					gettimeofday(&tm_end, NULL);
 					scs = (tm_end.tv_sec - tm_begin.tv_sec);
-					curr_tms = (scs*1e6)+tm_end.tv_usec-tm_begin.tv_usec;
-					
+					curr_tms = (scs * 1e6) + tm_end.tv_usec - tm_begin.tv_usec;
+
 					diff = curr_tms - t_ms;
-					t_ms = 1e6*Min_Simul;
-					if(diff < t_ms)	t_ms = t_ms - diff;
-				} else Hour_Simul++;
-				
+					t_ms = 1e6 * Min_Simul;
+					if (diff < t_ms)
+						t_ms = t_ms - diff;
+				}
+				else
+					Hour_Simul++;
+
 				// Aumento de tiempo para probar el codigo.
 				cnt++;
-				if(cnt == 15) Hour_Simul = Hour_Final;
+				if (cnt == 30)
+					Hour_Simul = Hour_Final;
 			}
 
 			close(files_desc[1][1]);
-			close(files_desc[PROCESS_ID+1][0]); // Cerramos el ultimo pipe escritura.
+			close(files_desc[PROCESS_ID + 1][0]); // Cerramos el ultimo pipe escritura.
 		}
 		return EXIT_SUCCESS;
 	}
@@ -124,98 +165,204 @@ int main(int argc, char *argv[]){
 /*   Definiciones y encabezados de funciones en "standard_lib.h"   */
 /*******************************************************************/
 
-void *showBus(void *data){
-	struct services *bus = (struct services *) data;
-	int timer = getMinutesOfBusWithMinutesAndHours(bus->leaveing) - 1;
+void *showBus(void *data)
+{
+	struct services *bus = (struct services *)data;
+	int leavingTime = getMinutesOfBusWithMinutesAndHours(bus->leaveing) - 1;
+	int timer = leavingTime;
+	int routeTimeOfBus = bus->travel_time;
+	int busAlreadyArrived = 0;
 
-	while(TRUE){
-		if(timer < Hour_Simul){
-			timer = Hour_Simul;
-			//printf("%s ", bus->code);
-			//printf("%d \n", Hour_Simul);
+	while (TRUE)
+	{
+		if (timer < Hour_Simul)
+		{
+
+			if (leavingTime + routeTimeOfBus > Hour_Simul)
+			{
+				bus->progressPercentage = getPercentageOfNumber(timer - leavingTime, routeTimeOfBus);
+			}
+			else if (leavingTime + routeTimeOfBus + 10 > Hour_Simul)
+			{
+				if (busAlreadyArrived == 0)
+				{
+					amountOfBusesGoingToBusStation[bus->charge_id]--;
+					amountOfBusesWaitingForPeople[bus->charge_id]++;
+					bus->progressPercentage = 0;
+					bus->isWaitingForPeople = 1;
+					busAlreadyArrived = 1;
+				}
+			}
+			else
+			{
+				amountOfBusesWaitingForPeople[bus->charge_id]--;
+				amountOfBusesGoingToUniversity[bus->charge_id]++;
+				bus->isWaitingForPeople = 0;
+				bus->isReturningToUniversity = 1;
+				break;
+			}
 			timer++;
 		}
-
 	}
-
 }
 
-void child_funtion(int ID, int pipes[][2]){
-	close(pipes[ID][1]);   // Cerramos escritura pipe izquierdo.
-	close(pipes[ID+1][0]); // Cerramos lectura pipe derecho.
+void child_funtion(int ID, int pipes[][2])
+{
+	close(pipes[ID][1]);	 // Cerramos escritura pipe izquierdo.
+	close(pipes[ID + 1][0]); // Cerramos lectura pipe derecho.
 	int minutes = 0;
 	char buf[10];
-			
-	while(TRUE){
+	int amountOfBusesGoingToBusStationInCurrentProcess = 0;
+	int amountOfBusesGoingToUniversityInCurrentProcess = 0;
+	int amountOfBusesWaitingForPeopleInCurrentProcess = 0;
+	int amountOfBusesSentInCurrentRoute = 0;
+
+	while (TRUE)
+	{
 		read(pipes[ID][0], buf, 10);
 		sscanf(buf, "%d", &minutes);
 		Hour_Simul = minutes;
- 
+
 		// HILOS A EJECUTAR//
 
-		int amountOfBusesSentInCurrentRoute = amountOfBusesUsedByRoute[ID];
+		int positionInServiceMatrixOfCurrentProcess = servicePositionInMatrixByRoute[ID];
 
-
-
-		if(minutes == getMinutesOfBusWithMinutesAndHours(total_ser[ID][amountOfBusesSentInCurrentRoute].leaveing) ){
-			pthread_create(&listOfPthreads[ID][amountOfBusesSentInCurrentRoute], NULL, &showBus, (void*) &total_ser[ID][amountOfBusesSentInCurrentRoute]);
-
+		if (minutes == getMinutesOfBusWithMinutesAndHours(total_ser[positionInServiceMatrixOfCurrentProcess][amountOfBusesSentInCurrentRoute].leaveing))
+		{
+			pthread_create(&listOfPthreads[ID][amountOfBusesSentInCurrentRoute], NULL, &showBus, (void *)&total_ser[positionInServiceMatrixOfCurrentProcess][amountOfBusesSentInCurrentRoute]);
 			amountOfBusesUsedByRoute[ID]++;
-			amountOfBusesCurrentlyUsedByRoute[ID]++;
+			amountOfBusesGoingToBusStation[ID]++;
 		}
 
-		write(pipes[ID+1][1], buf, 10);
-		if(minutes == -1) break;
+		amountOfBusesSentInCurrentRoute = amountOfBusesUsedByRoute[ID];
+		amountOfBusesGoingToBusStationInCurrentProcess = amountOfBusesGoingToBusStation[ID];
+		amountOfBusesGoingToUniversityInCurrentProcess = amountOfBusesGoingToUniversity[ID];
+		amountOfBusesWaitingForPeopleInCurrentProcess = amountOfBusesWaitingForPeople[ID];
+
+		if (amountOfBusesGoingToBusStationInCurrentProcess > 0 ||
+			amountOfBusesGoingToUniversityInCurrentProcess > 0 ||
+			amountOfBusesWaitingForPeopleInCurrentProcess > 0)
+		{
+			printf("%s: ", total_cha[ID].code);
+		}
+
+		FOR(n, 0, amountOfBusesWaitingForPeopleInCurrentProcess)
+		{
+			printf("[..........] ");
+		}
+
+		for (int i = 0; i < amountOfBusesGoingToBusStationInCurrentProcess; i++)
+		{
+			print_bus(total_ser[positionInServiceMatrixOfCurrentProcess][i].progressPercentage, 0);
+		}
+
+		/*FOR(n, 1, amountOfBusesWaitingForPeople[ID])
+		{
+		}*/
+
+		if (amountOfBusesGoingToBusStation[positionInServiceMatrixOfCurrentProcess] > 0 ||
+			amountOfBusesGoingToUniversity[positionInServiceMatrixOfCurrentProcess] > 0 ||
+			amountOfBusesWaitingForPeople[positionInServiceMatrixOfCurrentProcess] > 0)
+		{
+			printf(" \n");
+		}
+
+		write(pipes[ID + 1][1], buf, 10);
+		if (minutes == -1)
+			break;
 	}
 	close(pipes[ID][0]);
-	close(pipes[ID+1][0]);
+	close(pipes[ID + 1][0]);
 	exit(0);
 }
 
-
-void tracker_hour(int hour){
-	int h = hour/60;
+void tracker_hour(int hour)
+{
+	int h = hour / 60;
 	int m = hour % 60;
 	char format[6];
 	time_t now = time(NULL);
-	struct tm* t_a = localtime(&now);
+	struct tm *t_a = localtime(&now);
 	t_a->tm_hour = h;
 	t_a->tm_min = m;
 	strftime(format, 6, "%H:%M", t_a);
 	printf("Simulation Time: %s\n", format);
 }
 
-int getMinutesOfBusWithMinutesAndHours(struct time_b leavingTimeOfCurrentBus){
+int getPercentageOfNumber(int a, int b)
+{
+	if ((a * 100) / b >= 0)
+	{
+		return a * 100 / b;
+	}
+	return 100;
+}
+
+int getMinutesOfBusWithMinutesAndHours(struct time_b leavingTimeOfCurrentBus)
+{
 	return leavingTimeOfCurrentBus.hour * 60 + leavingTimeOfCurrentBus.min;
 }
 
-void ReadCacCharge(){
-	int rows = num_of_lines(charge_file); 
+void print_bus(int percentage, int direction)
+{
+	int rest;
+	int count = percentage / 10;
+	printf("[");
+	if (direction == 0)
+	{
+		for (int i = 0; i < count; ++i)
+		{
+			printf("<");
+		}
+	}
+	else
+	{
+		for (int i = 0; i < count; ++i)
+		{
+			printf(">");
+		}
+	}
+	rest = 10 - count;
+	for (int i = 0; i < rest; ++i)
+	{
+		printf(" ");
+	}
+	printf("] ");
+}
+
+void ReadCacCharge()
+{
+	int rows = num_of_lines(charge_file);
 	int c, hours, mins;
 	char buf[BUFFER_SIZE];
 	char *ptr;
 	struct charge *aux;
 
 	// Iteramos por las filas del archivo.
-	FOR(r, 0, rows){
+	FOR(r, 0, rows)
+	{
 		aux = &total_cha[r];
-		if(fgets(buf, sizeof(buf), charge_file)){
+		if (fgets(buf, sizeof(buf), charge_file))
+		{
 			aux->empty = 1;
 			ptr = strtok(buf, ",");
 			c = 0;
 			// Iteramos por las columnas.
-			while(ptr != NULL){
-				if(r == 0){
-					while(c++ < 3)
+			while (ptr != NULL)
+			{
+				if (r == 0)
+				{
+					while (c++ < 3)
 						ptr = strtok(NULL, ",");
-					
-					if(first_arrival == 0)
+
+					if (first_arrival == 0)
 						first_arrival = atoi(ptr);
 
 					last_arrival = atoi(ptr);
 					ptr = strtok(NULL, ",");
-
-				}	else {
+				}
+				else
+				{
 					// Por cada parada leida aumentamos el numero de procesos.
 					num_of_process++;
 
@@ -226,10 +373,11 @@ void ReadCacCharge(){
 					ptr = strtok(NULL, ",");
 
 					sscanf(ptr, " %d:%d", &hours, &mins);
-					aux->min_travel = hours*60 + mins;
+					aux->min_travel = hours * 60 + mins;
 					ptr = strtok(NULL, ",");
 
-					for(int k = first_arrival; k-1 < last_arrival; k++){
+					for (int k = first_arrival; k - 1 < last_arrival; k++)
+					{
 						aux->queue_per[k] = atoi(ptr);
 						ptr = strtok(NULL, ",");
 					}
@@ -240,7 +388,8 @@ void ReadCacCharge(){
 	fclose(charge_file); // Cerrar el archivo.
 }
 
-void ReadCacService(){
+void ReadCacService()
+{
 	int rows = num_of_lines(services_file);
 	int c, hours, mins, cap;
 	char buf[BUFFER_SIZE];
@@ -250,36 +399,48 @@ void ReadCacService(){
 	int f;
 
 	// Iteramos por las filas del archivo.
-	FOR(r, 1, rows){
-		c = 0; f = 0;
-		if(fgets(buf, sizeof(buf), services_file)){
+	FOR(r, 1, rows)
+	{
+		c = 0;
+		f = 0;
+		if (fgets(buf, sizeof(buf), services_file))
+		{
 			ptr = strtok(buf, " ");
 
 			strncpy(cod, ptr, 4);
 			ptr = strtok(NULL, " ");
-			
+
 			// Verificamos que coincidan los CODES.
-			if(strcmp(total_cha[r].code, cod)){
+			if (strcmp(total_cha[r].code, cod))
+			{
 				// Buscamos la fila correspondiente al CODE de carga.
-				FOR(x, 1, num_of_process){
-					if(strcmp(total_cha[x].code, cod)) continue;
-					f = x; break;	
-				}	
-			} else { f = r; }
-			
+				FOR(x, 1, num_of_process)
+				{
+					if (strcmp(total_cha[x].code, cod))
+						continue;
+					f = x;
+					break;
+				}
+			}
+			else
+			{
+				f = r;
+			}
+
 			// Verificar si la estructura esta vacia.
-			if(!total_cha[f].empty) continue;
+			if (!total_cha[f].empty)
+				continue;
 
 			// Iteramos por las columnas.
-			while( ptr != NULL){
+			while (ptr != NULL)
+			{
 				total_ser[f][c].empty = 1;
 				strncpy(total_ser[f][c].code, cod, 4);
 				sscanf(ptr, " %d:%d(%d)", &hours, &mins, &cap);
-				
-				// Verificamos la hora inicial simulada.
-				Hour_Simul = min(hours*60 + mins, Hour_Simul);
-				Hour_Final = max(hours*60 + mins + 2*total_cha[f].min_travel+10, Hour_Final);
 
+				// Verificamos la hora inicial simulada.
+				Hour_Simul = min(hours * 60 + mins, Hour_Simul);
+				Hour_Final = max(hours * 60 + mins + 2 * total_cha[f].min_travel + 10, Hour_Final);
 
 				total_ser[f][c].leaveing.hour = hours;
 				total_ser[f][c].leaveing.min = mins;
@@ -290,67 +451,78 @@ void ReadCacService(){
 		}
 	}
 
-	Hour_Simul -= 5; // Iniciamos 5 minutos antes.
+	Hour_Simul -= 5;	   // Iniciamos 5 minutos antes.
 	fclose(services_file); // Cerramos el archivo.
 }
 
-int num_of_lines(FILE *file){
+int num_of_lines(FILE *file)
+{
 	int rows = 0;
 	char buf[BUFFER_SIZE];
 	fseek(file, 0, SEEK_SET);
-	while(fgets(buf, BUFFER_SIZE, file))
+	while (fgets(buf, BUFFER_SIZE, file))
 		rows++;
 	fseek(file, 0, SEEK_SET);
 	return rows;
 }
 
-void initial_structs(){
+void initial_structs()
+{
 	// Init de "total_cha" con identificador vacio.
 	FOR(i, 0, n_routes)
-		total_cha[i].empty = 0;
-	
+	total_cha[i].empty = 0;
+
 	// Init de "total_set" con identificador vacio.
-	FOR(r, 0, n_routes){
+	FOR(r, 0, n_routes)
+	{
 		FOR(c, 0, max_bus)
-			total_ser[r][c].empty = 0;
+		total_ser[r][c].empty = 0;
 	}
 }
 
-void open_files(int argc, char *argv[]){
+void open_files(int argc, char *argv[])
+{
 	// Se ingreso solo el archivo "Caracteritica de Servicio".
-	if(argc == 2){
+	if (argc == 2)
+	{
 		services_file = fopen(argv[1], "r");
 		charge_file = fopen("carga.csv", "r");
-		if(charge_file == NULL || services_file == NULL)
+		if (charge_file == NULL || services_file == NULL)
 			perror("Error: Fail to open files.");
-	
-	// Se ingreso archivo "Caracteristica de Servicio" mas entero 
-	// "Tiempo a simular" o archivo "Caracteristica de Carga".
-	} else if(argc == 3) {
+
+		// Se ingreso archivo "Caracteristica de Servicio" mas entero
+		// "Tiempo a simular" o archivo "Caracteristica de Carga".
+	}
+	else if (argc == 3)
+	{
 		services_file = fopen(argv[1], "r");
-		
+
 		charge_file = fopen(argv[2], "r");
-		if(charge_file == NULL){
+		if (charge_file == NULL)
+		{
 			charge_file = fopen("carga.csv", "r");
 			sscanf(argv[2], "%f", &Min_Simul);
 		}
-		if(charge_file == NULL || services_file == NULL)
+		if (charge_file == NULL || services_file == NULL)
 			perror("Error: Fail to open files.");
 
-	// Se ingreso ambos archivos mas el tiempo a simular.
-	} else {
+		// Se ingreso ambos archivos mas el tiempo a simular.
+	}
+	else
+	{
 		sscanf(argv[3], "%f", &Min_Simul);
 		services_file = fopen(argv[1], "r");
 		charge_file = fopen(argv[2], "r");
-		if(charge_file == NULL || services_file == NULL)
+		if (charge_file == NULL || services_file == NULL)
 			perror("Error: Fail to open files.");
 	}
 }
 
-void ErrorArgument(int argc, char *argv[]){
+void ErrorArgument(int argc, char *argv[])
+{
 	printf("Error: Number/Names or arguments is incorrect.\n");
 	printf("Usage: %s", argv[0]);
-	for(int k=1; k < argc; k++)
+	for (int k = 1; k < argc; k++)
 		printf("%s ", argv[k]);
 	printf("\nCorrect: %s servicio.txt carga.csv\n", argv[0]);
 }
--- standard_lib.h
+++ standard_lib.h
@@ -9,49 +9,48 @@
 #include <unistd.h>
 
 // Directivas For.
-#define FOR(i, x, n) for(int i = x; i-x < n; i++)
+#define FOR(i, x, n) for (int i = x; i - x < n; i++)
 
 // Macros True and False.
 #ifndef TRUE
-	#define TRUE (0==0)
+#define TRUE (0 == 0)
 #endif
 #ifndef FALSE
-	#define FALSE !TRUE
+#define FALSE !TRUE
 #endif
 
 // Definicion de funcion min/max para dos valores x e y.
-#define min(x,y) \
+#define min(x, y) \
 	({ typeof (x) _x = (x); \
 			typeof (y) _y = (y); \
 			_x < _y ? _x : _y; })
-#define max(x,y) \
+#define max(x, y) \
 	({ typeof (x) _x = (x); \
 			typeof (y) _y = (y); \
 			_x > _y ? _x : _y; })
 
 // Buffers y Constantes Macros.
 #define BUFFER_SIZE 1024 // Buffer para lectura de filas en archivo.
-#define n_routes 50      // Numero maximo de rutas en la universidad.
-#define max_hour 24      // Maximo de horas en un dia.
-#define max_bus 200      // Maximo de Buses en una ruta.
+#define n_routes 50		 // Numero maximo de rutas en la universidad.
+#define max_hour 24		 // Maximo de horas en un dia.
+#define max_bus 200		 // Maximo de Buses en una ruta.
 
-FILE 	*charge_file,      // Variable para archivo de carga
-			*services_file;    // Variable para archivo de servicio.
-int first_arrival = 0;   // Hora Inicial de llegada de estudiantes.
-int last_arrival = 23;   // Hora final de llegada de etudiantes.
-int num_of_process = 0;  // Numero dep rocesos(Paradas) a crear.
-int Hour_Simul = 1440;   // Hora degault para la simulacion.
-int Hour_Final = 0;      // Hora final de la simulacion.
-float Min_Simul = 0.25;  // Minuto default para la simulacion.
-int PROCESS_ID = 0;      // Identificador de los procesos creados.
-
+FILE *charge_file,		// Variable para archivo de carga
+	*services_file;		// Variable para archivo de servicio.
+int first_arrival = 0;	// Hora Inicial de llegada de estudiantes.
+int last_arrival = 23;	// Hora final de llegada de etudiantes.
+int num_of_process = 0; // Numero dep rocesos(Paradas) a crear.
+int Hour_Simul = 1440;	// Hora degault para la simulacion.
+int Hour_Final = 0;		// Hora final de la simulacion.
+float Min_Simul = 0.25; // Minuto default para la simulacion.
+int PROCESS_ID = 0;		// Identificador de los procesos creados.
 
 // Estructura para guardar los tiempos usados en simulacion.
-struct time_b{
+struct time_b
+{
 	int hour;
 	int min;
 };
-
 
 //  Estructura para guardar la informacion del archivo de carga.
 //	- empty: Indica si la estructura esta en uso.
@@ -59,7 +58,8 @@ struct time_b{
 //	- code: Codigo de la parada.
 //	- name: Nombre de la parada.
 //	- min_travel: Tiempo de viaje del bus.
-struct charge{
+struct charge
+{
 	int empty;
 	int queue_per[max_hour];
 	char code[4];
@@ -71,33 +71,44 @@ struct charge{
 //	- empty:Indica si la estructura esta en uso.
 //	- code: Codigo de la parada.
 //	- leaveing: Hora de salida del bus desde la universidad.
-//	- c_capacity: Capacidad de carga del bus. 
+//	- c_capacity: Capacidad de carga del bus.
 //  - travel_time: Tiempo que tarda el bus en cumplir su ruta
-struct services{
+struct services
+{
 	int empty;
 	char code[4];
 	struct time_b leaveing;
 	int c_capacity;
 	int travel_time;
-	int service_id;
+	int charge_id;
+	int numberOfBusInRoute;
 	int progressPercentage;
 	int isWaitingForPeople;
 	int isReturningToUniversity;
 };
 
-/*struct progressOfBus{
+struct progressOfBus
+{
 	int progressPercentage;
 	int isWaitingForPeople;
 	int isReturningToUniversity;
-};*/
+};
 
 int amountOfBusesUsedByRoute[n_routes];
 
 int amountOfBusesFinishedByRoute[n_routes];
 
-int amountOfBusesCurrentlyUsedByRoute[n_routes];
+int amountOfBusesGoingToBusStation[n_routes];
 
-// Arreglo de unidimensional de estructuras de chargas. Permite 
+int amountOfBusesGoingToUniversity[n_routes];
+
+int amountOfBusesWaitingForPeople[n_routes];
+
+int servicePositionInMatrixByRoute[n_routes];
+
+int routePositionInMatrixByService[n_routes];
+
+// Arreglo de unidimensional de estructuras de chargas. Permite
 // indexar por ruta leida en archivo de carga. Cada indice contiene
 // la informacion por ruta, codigo, la cola de persona y toda informacion
 // de la parada.
@@ -108,31 +119,33 @@ struct charge total_cha[n_routes];
 // parada. Cada columna contiene la informacion de un bus respectivo.
 struct services total_ser[n_routes][max_bus];
 
+struct progressOfBus matrixWithProgressOfBus[n_routes][max_bus];
+
 pthread_t listOfPthreads[n_routes][max_bus];
 
 /***********************************************************************/
 
-// Funcion descrita para el trabajo de un proceso hijo creado por 
-// fork. 
-void child_funtion(int, int [][2]);
+// Funcion descrita para el trabajo de un proceso hijo creado por
+// fork.
+void child_funtion(int, int[][2]);
 
 // Funcion encargada de mostrar en consola la hora simulada dado un
 // numero entero que representa la cantidad de minutos.
 void tracker_hour(int);
 
-// Funcion encargada de obtener/extraer la informacion del 
-// archivo "Caracteristica de Carta" a la estructura 
+// Funcion encargada de obtener/extraer la informacion del
+// archivo "Caracteristica de Carta" a la estructura
 // "total_cha".
 void ReadCacCharge();
 
 // Funcion encargada de obtener/extraer la informacion del
-// archivo "Caracteristica de Servicio" a la estructura 
+// archivo "Caracteristica de Servicio" a la estructura
 // "total_ser".
 void ReadCacService();
 
 // Funcion encargada de obtener la cantidad de lineas de un
 // para un archivo dado.
-int num_of_lines(FILE*);
+int num_of_lines(FILE *);
 
 // Funcion encargada de inicializar todas las estructuras macros
 // de informacion como "total_cha" y "total_set" en sub_estructuras
@@ -141,10 +154,14 @@ void initial_structs();
 
 // Funcion encargada de abrir los archivos dados como argumento
 // en la funcion main.
-void open_files(int, char**);
+void open_files(int, char **);
 
 // Funcion encargada de mostrar errores de llamado al programa.
 // Cuando no se ingresan los archivos o la cantidad correcta.
-void ErrorArgument(int, char**);
+void ErrorArgument(int, char **);
 
 int getMinutesOfBusWithMinutesAndHours(struct time_b);
+
+void print_bus(int, int);
+
+int getPercentageOfNumber(int, int);
